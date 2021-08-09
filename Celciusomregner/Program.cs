791c42c7fb95a4121e0e16fc34d9649bb8f4267c
Grootrix <grootrix@gmail.com>
2021-08-09 11:44:26 +0200

    Add project files.

---
+++ Celciusomregner/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Celciusomregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opgave A
+            // Celciusomregner
+
+            Console.WriteLine("Write a number: ");
+            double r = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Write a number: ");
+            double f = double.Parse(Console.ReadLine());
+
+            double a = r * 0.8;
+            double b = f * 1.8 + 32;
+
+            Console.WriteLine("Reamur er: " + a + " og Fahrenheit er " + b );
+
+
+            //Prevents the program for shutting down.
+            Console.ReadKey();
+        }
+    }
+}
